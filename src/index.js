7bd5911663effa19e19fc824e25c9fa57f2f62fd
Modeste <mn84@students.uwf.edu>
2024-04-27 13:06:18 -0500

    Added Firebase

--- src/index.js
+++ src/index.js
@@ -9,6 +9,31 @@ import BookList from "./components/BookList/BookList";
 import BookDetails from "./components/BookDetails/BookDetails";
 import FavoriteBooks from './pages/FavoriteBooks/FavoriteBooks';
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB46sCe32E_PbEOwH9yXIeI_x-Bt_CqGcs",
+  authDomain: "novelus-2024.firebaseapp.com",
+  projectId: "novelus-2024",
+  storageBucket: "novelus-2024.appspot.com",
+  messagingSenderId: "36070124762",
+  appId: "1:36070124762:web:57b0e7e23089c26c0f09e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <AppProvider>
